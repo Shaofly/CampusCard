4901805a6f7b3383185cc353300c7914155ce0ce
msf <1309331756@qq.com>
2025-06-06 02:19:11 +0800

    添加了一个设置语言的界面和两个简单的国际化资源文件：message_zh_CN和message_en。

---
+++ src/com/mag/servlet/ChangeLanguageServlet.java
@@ -0,0 +1,32 @@
+package com.mag.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.Locale;
+
+public class ChangeLanguageServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // 获取用户选择的语言
+        String language = request.getParameter("language");
+
+        // 根据选择创建 Locale 对象
+        Locale locale;
+        if ("zh".equals(language)) {
+            locale = new Locale("zh", "CN");
+        } else {
+            locale = new Locale("en");
+        }
+
+        // 将 Locale 保存到 Session 中
+        HttpSession session = request.getSession();
+        session.setAttribute("locale", locale);
+
+        // 重定向回设置页面或首页
+        response.sendRedirect("settings.jsp");
+    }
+}
